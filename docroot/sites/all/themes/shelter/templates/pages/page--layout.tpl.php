a84dae75f000e382117c0051a8d7040e69884e45
Jonathan Harvey <jonathanh@appnovation.com>
2014-11-26 13:58:02 -0500

    header markup

--- docroot/sites/all/themes/shelter/templates/pages/page--layout.tpl.php
+++ docroot/sites/all/themes/shelter/templates/pages/page--layout.tpl.php
@@ -1,15 +1,49 @@
 <div id="page">
 
   <header>
+    <div class="page-margin">
+      <div id="bandwidth-selector">
+        <a>Low</a>
+        <a>High</a>
+      </div>
+      <div id="language-selector">
+        <ul class="languages">
+          <li class="language">en</li>
+          <li class="language">fr</li>
+          <li class="language">es</li>
+          <li class="language">ar</li>
+        </ul>
+      </div>
 
-  </header>
+      <div id="btn-dropdown-shelter">menu</div>
+      <ul id="profile-items">
+        <li class="profile-item"><a>Login</a></li>
+        <li class="profile-item"><a>Create an account</a></li>
+      </ul>
+      <nav id="nav-shelter">
+        <ul id="nav-items">
+          <li class="nav-item"><a>Home</a></li>
+          <li class="nav-item"><a>Search Documents <span class="total">(8 200)</span></a></li>
+          <li class="nav-item"><a>Regional Groups <span class="total">(6)</span></a></li>
+          <li class="nav-item"><a>Countries <span class="total">(34)</span></a></li>
+          <li class="nav-item"><a>Crises <span class="total">(3)</span><span class="total-ended">(20) ended</span></a></li>
+          <li class="nav-item"><a>Manage your profile</a></li>
+          <li class="nav-item"><a>Disconnect</a></li>
+        </ul>
+      </nav>
+      <a id="logo-shelter">
+        <div id="title-shelter">ShelterCluster.org<div>
+        <div id="subtitle-shelter">Coordinating Humanitarian Shelter<div>
+      </a>
+    </header>
 
-  <div id="content">
+    <div id="content">
 
-  </div>
+    </div>
 
-  <footer>
+    <footer>
 
-  </footer>
+    </footer>
 
+  <div>
 <div>
