732c00681fd5253c553793bba0c1328346a27a79
Antoine Lafontaine <antoine@appnovation.com>
2014-11-26 13:41:31 -0500

    First pass at the footer markup and layout

--- docroot/sites/all/themes/shelter/templates/pages/page--layout.tpl.php
+++ docroot/sites/all/themes/shelter/templates/pages/page--layout.tpl.php
@@ -9,7 +9,68 @@
   </div>
 
   <footer>
+    <div class="page-margin">
 
+      <section id="active-clusters-list">
+        <h3>With 24 <a href='#'>active shelter clusters</a> and cluster like mechanism</h3>
+        <ul class="active-clusters-list-clusters">
+          <li class="active-clusters-list-cluster"><a href="#">Myanmar Rakhine and Kachin Emergency Response</a></li>
+          <li class="active-clusters-list-cluster"><a href="#">Philippines Bohol Earthquake</a></li>
+          <li class="active-clusters-list-cluster"><a href="#">Solomon Islands Floods 2014</a></li>
+        </ul>
+        <a class="active-clusters-complete-list" href="#">[...] more</a>
+      </section><section id="regions-list">
+        <h3>Shelter Cluster is present in over <a href="#">34 countries</a></h3>
+        <ul class="regions">
+          <li class="regions-region"><a href="#">Africa</a>
+            <ul class="regions-region-countries">
+              <li class="regions-region-countries-country"><a href="#">Central African Republic</a></li>
+              <li class="regions-region-countries-country"><a href="#">Chad</a></li>
+            </ul>
+          </li>
+
+          <li class="regions-region"><a href="#">Americas</a>
+            <ul class="regions-region-countries">
+              <li class="regions-region-countries-country"><a href="#">Colombia</a></li>
+              <li class="regions-region-countries-country"><a href="#">Haiti</a></li>
+            </ul>
+          </li>
+
+          <li class="regions-region"><a href="#">MENA</a>
+            <ul class="regions-region-countries">
+              <li class="regions-region-countries-country"><a href="#">Iraq</a></li>
+              <li class="regions-region-countries-country"><a href="#">Palestine</a></li>
+              <li class="regions-region-countries-country"><a href="#">Yemen</a></li>
+            </ul>
+          </li>
+        </ul>
+      </section>
+
+      <section id="general-information">
+        <h3>General Information</h3>
+        <ul class="general-information-links">
+          <li class="general-information-link"><a href="#">About this site</a></li>
+          <li class="general-information-link"><a href="#">Contact information</a></li>
+          <li class="general-information-link"><a href="#">Twitter account</a></li>
+        </ul><ul class="general-information-links">
+          <li class="general-information-link"><a href="#">Support Team</a></li>
+          <li class="general-information-link"><a href="#">Partnership</a></li>
+        </ul><ul class="general-information-links">
+          <li class="general-information-link"><a href="#">Other</a></li>
+          <li class="general-information-link"><a href="#">More information</a></li>
+          <li class="general-information-link"><a href="#">Twitter account</a></li>
+        </ul>
+      </section>
+
+      <section id="partners-list">
+        <ul class="partners-list-partners clearfix">
+          <li class="partners-list-partner"><a href="#">ECHO</a></li>
+          <li class="partners-list-partner"><a href="#">IFRC</a></li>
+          <li class="partners-list-partner"><a href="#">UNHCR</a></li>
+        </ul>
+      </section>
+
+    </div>
   </footer>
 
 <div>
