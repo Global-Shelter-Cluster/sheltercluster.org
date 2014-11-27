deb1c28e3e8e71ce3af8af2cbad39510ea5d8bc2
Antoine Lafontaine <antoine@appnovation.com>
2014-11-27 14:01:44 -0500

    Footer style tweaks

--- docroot/sites/all/themes/shelter/templates/pages/page--layout.tpl.php
+++ docroot/sites/all/themes/shelter/templates/pages/page--layout.tpl.php
@@ -60,17 +60,17 @@
       </section><section id="regions-list">
         <h3>Shelter Cluster is present in over <a href="#">34 countries</a></h3>
         <ul class="regions">
-          <li class="region"><a href="#">Africa</a>
+          <li class="region"><a href="#" class="region-name">Africa</a>
             <ul class="countries">
               <li class="country"><a href="#">Central African Republic</a></li>
               <li class="country"><a href="#">Chad</a></li>
             </ul>
-          </li><li class="region"><a href="#">Americas</a>
+          </li><li class="region"><a href="#" class="region-name" >Americas</a>
             <ul class="countries">
               <li class="country"><a href="#">Colombia</a></li>
               <li class="country"><a href="#">Haiti</a></li>
             </ul>
-          </li><li class="region"><a href="#">MENA</a>
+          </li><li class="region"><a href="#" class="region-name">MENA</a>
             <ul class="countries">
               <li class="country"><a href="#">Iraq</a></li>
               <li class="country"><a href="#">Palestine</a></li>
