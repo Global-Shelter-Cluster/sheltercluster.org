43fd707926b72343dfdf93ea9d367cc5ae13a6b1
Jean-Michel Lavarenne <jeanmichel@berkano.local>
2014-11-29 11:31:18 -0500

    Modify GroupPageContent to hide node type manipulations from module hook_node_view implementation

--- docroot/sites/all/modules/custom/cluster_og/classes/GroupPageContent.php
+++ docroot/sites/all/modules/custom/cluster_og/classes/GroupPageContent.php
@@ -1,36 +1,8 @@
 <?php
 
-class GroupPageContent_Response extends GroupPageContent {
-  protected $parent_field = 'field_parent_response';
-
-  public function getRelatedResponses($view_mode = 'related_response') {
-    $nids = $this->getDescendantIds(FALSE);
-    return $this->getList($nids, $view_mode, 'cluster_og_related_responses');
-  }
-
-  public function getRelatedHubs($view_mode = 'related_hub') {
-    $nids = $this->queryChildren($this->getDescendantIds(TRUE), 'field_parent_response', 'hub');
-    return $this->getList($nids, $view_mode, 'cluster_og_related_hubs');
-  }
-}
-
-class GroupPageContent_GeographicRegion extends GroupPageContent {
-  protected $parent_field = 'field_parent_region';
-
-  public function getRelatedResponses($view_mode = 'related_response') {
-    $nids = self::queryChildren($this->getDescendantIds(TRUE), 'field_associated_regions', 'response');
-    return self::getList($nids, $view_mode, 'cluster_og_related_responses');
-  }
-
-  public function getRelatedHubs($view_mode = 'related_hub') {
-    $nids = self::queryChildren($this->getDescendantIds(TRUE), 'field_parent_region', 'hub');
-    return self::getList($nids, $view_mode, 'cluster_og_related_hubs');
-  }
-}
-
-class GroupPageContent {
-
-  protected $node;
+interface GroupContent {
+  public function getRelatedResponses($view_mode);
+  public function getRelatedHubs($view_mode);
 
   /**
    * @var Entity reference field name used to get children for the node.
@@ -38,53 +10,6 @@ class GroupPageContent {
    * $this->getDescendantIds() work.
    */
   protected $parent_field = NULL;
-
-  /**
-   * @var Array of descendant IDs, stored for caching purposes in a single request.
-   */
-  protected $descendant_ids;
-
-  function __construct($node) {
-    $this->node = $node;
-  }
-
-  public function getContactMembers($view_mode = 'contact_member') {
-    $contact_members_ids = $this->getUsersByRole('contact member');
-    return self::getList($contact_members_ids, $view_mode, 'cluster_og_contact_member', 'user');
-  }
-
-  /**
-   * Get the role id for a group from the role name.
-   * @param $role_name
-   *  The role name as stored in the database.
-   * @return Integer representing the role ID.
-   */
-  private function getRoleIdByName($role_name) {
-    return db_select('og_role', 'og_r')
-      ->fields('og_r', array('rid'))
-      ->condition('group_bundle', $this->node->type)
-      ->condition('name', $role_name)
-      ->execute()->fetchField();
-  }
-
-  /**
-   * Get all users with specified role for a group.
-   * @param $role_name
-   *  The role name as stored in the database.
-   * @return Array of user IDs.
-   */
-  private function getUsersByRole($role_name) {
-    $rid = $this->getRoleIdByName($role_name);
-    if (!$rid) {
-      return;
-    }
-
-    return db_select('og_users_roles', 'og_ur')
-      ->fields('og_ur', array('uid'))
-      ->condition('gid', $this->node->nid)
-      ->condition('rid', $rid)
-      ->execute()->fetchCol();
-  }
 
   protected function getDescendantIds($include_self = FALSE, &$collected_nids = array()) {
     if (!$this->parent_field) {
@@ -99,9 +24,25 @@ class GroupPageContent {
       $ret = $this->descendant_ids;
       $ret[] = $this->node->nid;
       return $ret;
-    } else {
+    }
+    else {
       return $this->descendant_ids;
     }
+  }
+
+  /**
+   * Helper function. Queries the DB to find children of a specific content type,
+   * by parent ID.
+   * @param $parent_nids
+   *  Array of node IDs for which to find children.
+   * @param $field
+   *  Entity reference field name to use.
+   * @param $bundle
+   *  Content type to look for.
+   * @return Array of node IDs.
+   */
+  protected static function queryChildren($parent_nids, $field, $bundle) {
+    return self::queryDescendants($parent_nids, $field, $bundle, FALSE, TRUE);
   }
 
   /**
@@ -136,7 +77,8 @@ class GroupPageContent {
       // We found children nodes.
       if ($only_children) {
         $return_nids = array_keys($res['node']);
-      } else {
+      }
+      else {
         // Do a recursive call to get all descendants.
         $return_nids = self::queryDescendants(array_keys($res['node']), $field, $bundle, TRUE);
       }
@@ -148,28 +90,120 @@ class GroupPageContent {
       // We call array_unique just in case there are duplicates. There shouldn't.
       return array_unique($return_nids);
 
-    } elseif ($include_self) {
+    }
+    elseif ($include_self) {
       // No results from the query but we were asked to include parents in the result.
       return $parent_nids;
-    } else {
+    }
+    else {
       // No results and no parents are to be returned, so return empty.
       return array();
     }
   }
+}
+
+class GroupContentResponse implements GroupContent {
+  protected $parent_field = 'field_parent_response';
+
+  public function getRelatedResponses($view_mode = 'related_response') {
+    return $this->getDescendantIds(FALSE);
+    //return $this->getList($nids, $view_mode, 'cluster_og_related_responses');
+  }
+
+  public function getRelatedHubs($view_mode = 'related_hub') {
+    return $this->queryChildren($this->getDescendantIds(TRUE), 'field_parent_response', 'hub');
+    //return $this->getList($nids, $view_mode, 'cluster_og_related_hubs');
+  }
+}
+
+class GroupContentGeographicRegion implements GroupContent {
+  protected $parent_field = 'field_parent_region';
+
+  public function getRelatedResponses($view_mode = 'related_response') {
+    return self::queryChildren($this->getDescendantIds(TRUE), 'field_associated_regions', 'response');
+    //return self::getList($nids, $view_mode, 'cluster_og_related_responses');
+  }
+
+  public function getRelatedHubs($view_mode = 'related_hub') {
+    return self::queryChildren($this->getDescendantIds(TRUE), 'field_parent_region', 'hub');
+    //return self::getList($nids, $view_mode, 'cluster_og_related_hubs');
+  }
+}
+
+class GroupPageContent {
+
+  protected $node;
 
   /**
-   * Helper function. Queries the DB to find children of a specific content type,
-   * by parent ID.
-   * @param $parent_nids
-   *  Array of node IDs for which to find children.
-   * @param $field
-   *  Entity reference field name to use.
-   * @param $bundle
-   *  Content type to look for.
-   * @return Array of node IDs.
+   * @var Array of descendant IDs, stored for caching purposes in a single request.
    */
-  protected static function queryChildren($parent_nids, $field, $bundle) {
-    return self::queryDescendants($parent_nids, $field, $bundle, FALSE, TRUE);
+  protected $descendant_ids;
+
+  /**
+   * @var Implements GroupContent interface.
+   */
+  private $manager;
+
+  function __construct($node) {
+    $this->node = $node;
+
+    switch ($node->type) {
+      case 'geographic_region':
+        $this->manager = new GroupContentGeographicRegion();
+        break;
+      case 'response':
+        $this->manager = new GroupContentReponse();
+        break;
+    }
+
+  }
+
+  public function getContactMembers($view_mode = 'contact_member') {
+    $contact_members_ids = $this->getUsersByRole('contact member');
+    return self::getList($contact_members_ids, $view_mode, 'cluster_og_contact_member', 'user');
+  }
+
+  public function getRelatedResponses($view_mode = 'related_response') {
+    $nids = $this->manager->getRelatedResponses();
+    return self::getList($nids, $view_mode, 'cluster_og_related_responses');
+  }
+
+  public function getRelatedHubs($view_mode = 'related_hub') {
+    $nids =  $this->manager->getRelatedHubs();
+    return self::getList($nids, $view_mode, 'cluster_og_related_hubs');
+  }
+
+  /**
+   * Get the role id for a group from the role name.
+   * @param $role_name
+   *  The role name as stored in the database.
+   * @return Integer representing the role ID.
+   */
+  private function getRoleIdByName($role_name) {
+    return db_select('og_role', 'og_r')
+      ->fields('og_r', array('rid'))
+      ->condition('group_bundle', $this->node->type)
+      ->condition('name', $role_name)
+      ->execute()->fetchField();
+  }
+
+  /**
+   * Get all users with specified role for a group.
+   * @param $role_name
+   *  The role name as stored in the database.
+   * @return Array of user IDs.
+   */
+  private function getUsersByRole($role_name) {
+    $rid = $this->getRoleIdByName($role_name);
+    if (!$rid) {
+      return;
+    }
+
+    return db_select('og_users_roles', 'og_ur')
+      ->fields('og_ur', array('uid'))
+      ->condition('gid', $this->node->nid)
+      ->condition('rid', $rid)
+      ->execute()->fetchCol();
   }
 
   /**
