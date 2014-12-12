a5931ac698f3ba32fdef64ccf3647e79d670ecc2
Jean-Michel Lavarenne <jeanmichel@berkano.local>
2014-12-12 09:35:44 -0500

    remove variable print call

--- docroot/sites/all/themes/shelter/templates/html/html.tpl.php
+++ docroot/sites/all/themes/shelter/templates/html/html.tpl.php
@@ -12,7 +12,6 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 
 <head profile="<?php print $grddl_profile; ?>">
-  <?php print $polyfill_queries_path; ?>
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
