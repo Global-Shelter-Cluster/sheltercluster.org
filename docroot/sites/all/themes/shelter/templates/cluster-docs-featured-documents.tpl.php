be5e0e2082852aa70d8bee89b2ce77e1af4ecb33
Antoine Lafontaine <antoine@appnovation.com>
2014-12-19 09:41:40 -0500

    Featured document with no image

--- docroot/sites/all/themes/shelter/templates/cluster-docs-featured-documents.tpl.php
+++ docroot/sites/all/themes/shelter/templates/cluster-docs-featured-documents.tpl.php
@@ -9,10 +9,18 @@
         <?php print theme('image', array('path' => $doc['image_uri'])); ?>
       <?php elseif ($doc['has_preview']): ?>
         <?php print theme('image', array('path' => $doc['preview_uri'])); ?>
+      <?php else: ?>
+        <img src="http://placehold.it/1000x700/698595" />
       <?php endif; ?>
     </div>
     <div class="document-information">
-      <h2><?php print $doc['title']; ?></h2>
+      <?php if ($doc['is_image']): ?>
+        <h2><?php print $doc['title']; ?></h2>
+      <?php elseif ($doc['has_preview']): ?>
+        <h2><?php print $doc['title']; ?></h2>
+      <?php else: ?>
+        <h2><?php print l( _svg('icons/file', array('class'=>'document-external', 'alt' => 'Icon for an external resource')) . ' ' . $doc['title'], $doc['link_url'], array('html' => TRUE, 'attributes' => array('target' => '_blank'))); ?></h2>
+      <?php endif; ?>
       <?php print $doc['description']; ?>
     </div>
   </li>
