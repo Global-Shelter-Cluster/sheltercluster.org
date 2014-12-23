6d63636e072ca3736aac379962c0f6513988377c
Jean-Michel Lavarenne <jeanmichel@berkano.local>
2014-12-23 17:24:37 -0500

    join links

---
+++ docroot/sites/all/modules/custom/cluster_og/theme/cluster-og-join-links.tpl.php
@@ -0,0 +1,5 @@
+<h3 data-collapse="operation-group"><?php print $header; ?></h3>
+<section id="join-group" class="clearfix">
+  <p><?php print $instructions; ?></p>
+  <div id="button-join-group"><?php print $link;?></div>
+</section>
