cff8b1738351ce6c49bdd9ef7b7854d1a9a834e8
Jean-Michel Lavarenne <jeanmichel@berkano.local>
2015-01-30 15:36:45 -0500

    Change discussion page markup to not use <li> tags.

--- docroot/sites/all/themes/shelter/templates/nodes/node--discussion--full.tpl.php
+++ docroot/sites/all/themes/shelter/templates/nodes/node--discussion--full.tpl.php
@@ -11,7 +11,6 @@
   hide($content['og_group_ref']);
 ?>
 
-
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <div class="content"<?php print $content_attributes; ?>>
