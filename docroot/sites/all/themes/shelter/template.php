7e74ede89c9a1800348b28c2089055bbf8c94cf7
Antoine Lafontaine <antoine@appnovation.com>
2015-02-02 15:59:54 -0500

    Better current operations list markup

--- docroot/sites/all/themes/shelter/template.php
+++ docroot/sites/all/themes/shelter/template.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @file
+ * Shelter theme template.php.
+ */
+
 require_once dirname(__FILE__) . '/includes/shelter.helpers.inc';
 
 /**
@@ -88,7 +93,7 @@ function shelter_preprocess_node(&$variables) {
   $variables['edit_link'] = FALSE;
   // Add an edit link users having the appropriate permission.
   if (node_access('update', $node)) {
-    $variables['edit_link'] = l('Edit', 'node/' . $node->nid . '/edit');
+    $variables['edit_link'] = l(t('Edit'), 'node/' . $node->nid . '/edit');
   }
   $view_mode = $variables['view_mode'];
   $is_group = og_is_group('node', $node);
@@ -98,10 +103,9 @@ function shelter_preprocess_node(&$variables) {
   }
 
   // Adding view mode based theme suggestions and preprocesses.
-  $variables['theme_hook_suggestions'][] = 'node__partial__' . $variables['view_mode'];
   $variables['theme_hook_suggestions'][] = 'node__' . $node->type . '__' . $variables['view_mode'];
 
-  $view_mode_based_preprocess = 'shelter_preprocess_node_partial__' . $variables['view_mode'];
+  $view_mode_based_preprocess = 'shelter_preprocess_node' . $node->type . '_' . $variables['view_mode'];
   if (function_exists($view_mode_based_preprocess)) {
     $view_mode_based_preprocess($variables);
   }
@@ -145,25 +149,6 @@ function shelter_preprocess_node(&$variables) {
       $view_mode_based_preprocess($variables);
     }
   }
-
-}
-
-/**
- * Implements hook_preprocess_node__[view mode]().
- */
-function shelter_preprocess_node_partial__related_hub(&$variables) {
-  $node = $variables['node'];
-  $markup = _svg('icons/grid-three-up', array('alt' => 'Icon for Hubs')) . ' ' . $node->title;
-  $variables['link'] = l($markup, 'node/' . $node->nid, array('html' => TRUE));
-}
-
-/**
- * Implements hook_preprocess_node__[view mode]().
- */
-function shelter_preprocess_node_partial__related_response(&$variables) {
-  $node = $variables['node'];
-  $markup = _svg('icons/globe', array('alt' => 'Icon for Related Responses')) . ' ' . $node->title;
-  $variables['link'] = l($markup, 'node/' . $node->nid, array('html' => TRUE));
 }
 
 /**
@@ -198,37 +183,9 @@ function shelter_menu_link(array $variables) {
   return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
 }
 
-function shelter_preprocess_node_partial__contextual_navigation(&$variables) {
-  $node = $variables['node'];
-  try {
-    $node_wrapper = entity_metadata_wrapper('node', $node);
-
-    if (isset($node_wrapper->field_parent_response)) {
-      //dpm('response');
-    }
-    if (isset($node_wrapper->field_parent_region)) {
-      //dpm('region');
-    }
-    if (isset($node_wrapper->field_associated_regions )) {
-      //dpm('associated regions');
-    }
-
-    //dpm($node_wrapper->getPropertyInfo());
-  }
-  catch (EntityMetadataWrapperException $exception) {
-    _log_entity_metadata_wrapper_error($exception, 'po_promoted');
-  }
-}
-
-/**
- * Implements hook_preprocess_search_result().
- */
-function shelter_preprocess_search_result(&$variables) {
-  //dpm($variables);
-}
-
 /**
  * Implements hook suggestion for local block links.
+ *
  * Replace full language name with abreviation.
  */
 function shelter_links__locale_block(&$variables) {
@@ -247,7 +204,7 @@ function shelter_links__locale_block(&$variables) {
         // is a string.
         $heading = array(
           'text' => $heading,
-          
+
           // Set the default level of the heading.
           'level' => 'h2',
         );
