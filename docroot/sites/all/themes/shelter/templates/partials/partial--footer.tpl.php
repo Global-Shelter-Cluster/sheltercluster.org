239ef42b593731736a271822bc2948c65bde6eb9
Antoine Lafontaine <antoine@appnovation.com>
2015-02-26 23:58:39 -0500

    Footer and header fixes

--- docroot/sites/all/themes/shelter/templates/partials/partial--footer.tpl.php
+++ docroot/sites/all/themes/shelter/templates/partials/partial--footer.tpl.php
@@ -4,14 +4,14 @@
     <?php print render($search_form_bottom); ?>
 
     <section id="active-clusters-list">
-      <h3>Hot responses</h3>
+      <h3><?php print t('Hot responses'); ?></h3>
       <?php print render($page['footer']['hot_responses']); ?>
-      <h3 class="general-information">General Information</h3>
+      <h3 class="general-information"><?php print t('General Information'); ?></h3>
       <?php print render($page['footer']['general_information']); ?>
     </section>
 
     <section id="regions-list">
-      <h3>Shelter Cluster is present in many countries</a></h3>
+      <h3><?php print t('Shelter information is available on the following countries:'); ?></a></h3>
       <?php print render($page['footer']['menu_regions']); ?>
     </section>
 
