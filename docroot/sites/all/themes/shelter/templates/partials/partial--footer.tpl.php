c12e03d96b680f84c1a7ca9679cfa29b578c838a
Antoine Lafontaine <antoine@appnovation.com>
2015-02-26 23:19:34 -0500

    More hompeage improvements

--- docroot/sites/all/themes/shelter/templates/partials/partial--footer.tpl.php
+++ docroot/sites/all/themes/shelter/templates/partials/partial--footer.tpl.php
@@ -6,16 +6,13 @@
     <section id="active-clusters-list">
       <h3>Hot responses</h3>
       <?php print render($page['footer']['hot_responses']); ?>
+      <h3 class="general-information">General Information</h3>
+      <?php print render($page['footer']['general_information']); ?>
     </section>
 
     <section id="regions-list">
       <h3>Shelter Cluster is present in many countries</a></h3>
       <?php print render($page['footer']['menu_regions']); ?>
-    </section>
-
-    <section id="general-information">
-      <h3>General Information</h3>
-        <?php print render($page['footer']['general_information']); ?>
     </section>
 
   </div>
