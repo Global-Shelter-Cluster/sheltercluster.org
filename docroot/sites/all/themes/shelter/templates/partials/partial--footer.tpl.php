a7a78eab183cf13d61cc036d1cec129c37d9a1e0
Jonathan Harvey <jonathanh@appnovation.com>
2015-03-04 15:42:42 -0500

    Added contributors logo into the footer

--- docroot/sites/all/themes/shelter/templates/partials/partial--footer.tpl.php
+++ docroot/sites/all/themes/shelter/templates/partials/partial--footer.tpl.php
@@ -1,8 +1,6 @@
 <footer>
   <div class="page-margin inside-footer">
-
     <?php print render($search_form_bottom); ?>
-
     <section id="active-clusters-list">
       <h3><?php print t('Hot responses'); ?></h3>
       <?php print render($page['footer']['hot_responses']); ?>
@@ -11,9 +9,34 @@
     </section>
 
     <section id="regions-list">
-      <h3><?php print t('Shelter information is available on the following countries:'); ?></a></h3>
+      <h3><?php print t('Shelter information is available on the following countries:'); ?></h3>
       <?php print render($page['footer']['menu_regions']); ?>
     </section>
+  </div>
 
+  <div class="page-margin inside-footer">
+    <section id="contributions">
+      <h4><?php print t('This website is made possible through the financial and in-kind contributions of:'); ?></h4>
+      <ul class="contributors clearfix">
+        <li class="contributor">
+          <?php print theme('image', array(
+            'path' => drupal_get_path('theme', 'shelter') . '/assets/images/humanitarian-aid.png',
+            'alt' => t('Humanitarian Aid and Civil Protection'),
+          )); ?>
+        </li>
+        <li class="contributor">
+          <?php print theme('image', array(
+            'path' => drupal_get_path('theme', 'shelter') . '/assets/images/canadian-red-cross.png',
+            'alt' => t('Canadian Red Cross'),
+          )); ?>
+        </li>
+        <li class="contributor">
+          <?php print theme('image', array(
+            'path' => drupal_get_path('theme', 'shelter') . '/assets/images/gov-canada.png',
+            'alt' => t('Governement of Canada'),
+          )); ?>
+        </li>
+      </ul>
+    </section>
   </div>
 </footer>
