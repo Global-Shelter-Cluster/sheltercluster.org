73bb8d99394c05b40ad11d3d36b61c6ea05c93a4
Antoine Lafontaine <antoine@appnovation.com>
2015-03-10 14:20:04 -0400

    Homepage layout refactoring

--- docroot/sites/all/themes/shelter/templates/partials/partial--homepage.tpl.php
+++ docroot/sites/all/themes/shelter/templates/partials/partial--homepage.tpl.php
@@ -11,36 +11,24 @@
     <?php if($hot_responses): ?>
       <?php print render($hot_responses); ?>
     <?php endif; ?>
-    <section id="secondary-nav">
+
+    <section id="secondary-nav" class="clearfix">
       <h3><?php print t('Home'); ?></h3>
       <?php print $homepage_menu; ?>
     </section>
-  </div>
 
-  <div class="main-column clearfix">
-    <div>
-      <div class="video-container">
-        <iframe src="http://www.youtube.com/embed/nzGuN0Cpzks?controls=0&loop=1&autoplay=0" frameborder="0" width="460" height="315"></iframe>
-      </div>
-    </div>
-    <div class="wysiwyg">
-      <?php print render($page['content']); ?>
-    </div>
-  </div>
-
-</div>
-
-<div class="page-margin clearfix">
-
-  <div class="side-column">
     <?php if($upcoming_events): ?>
       <?php print render($upcoming_events); ?>
     <?php endif; ?>
+
     <?php print partial('twitter_timeline', array('widget_id' => '569895346130534400')); ?>
+
   </div>
 
-  <div class="main-column">
-    <?php print partial('posters'); ?>
+  <div class="main-column clearfix">
+    <div class="wysiwyg">
+      <?php print render($page['content']); ?>
+    </div>
   </div>
 
 </div>
