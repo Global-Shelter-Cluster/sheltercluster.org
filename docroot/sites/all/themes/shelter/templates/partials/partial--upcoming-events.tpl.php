6b78c79336855a7a9fa0c291c6a52d5b98c036ad
Antoine Lafontaine <antoine@appnovation.com>
2015-12-10 15:09:48 -0500

    Upcoming events style tweaks

--- docroot/sites/all/themes/shelter/templates/partials/partial--upcoming-events.tpl.php
+++ docroot/sites/all/themes/shelter/templates/partials/partial--upcoming-events.tpl.php
@@ -3,11 +3,29 @@
   <div id="box-calendar">
     <?php print _svg('icons/pin', array('id' => 'calendar-pin-icon', 'alt' => t('An icon representing a calendar with a pin on it.'))); ?>
     <div id="date-calendar"><?php print $title ?></div>
-    <div class="information-card">
-      <?php foreach($events as $event): ?>
-        <?php print $event['event_link']; ?>
-      <?php endforeach; ?>
+
+    <?php foreach($events as $event): ?>
+      <div class="event-card">
+        <div>
+          <?php print $event['link']; ?>
+        </div>
+        <div class="event-date">
+          <span class="label"><?php print t('Event date: '); ?></span>
+          <span><?php print $event['date']; ?></span>
+        </div>
+        <div class="event-location">
+          <span class="label"><?php print t('Location: '); ?></span>
+          <span><?php print $event['location']; ?></span>
+        </div>
+        <div class="event-contact">
+          <span class="label"><?php print t('Contact: '); ?></span>
+          <span><?php print $event['location']; ?></span>
+        </div>
+      </div>
+    <?php endforeach; ?>
+
+    <div class="all-events">
+      <?php print $all_events_link; ?>
     </div>
-    <?php print $all_events_link; ?>
   </div>
 </section>
