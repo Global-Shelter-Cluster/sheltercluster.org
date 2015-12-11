8187bf5bb499d05595aba325d66cf4f2b6d25ac1
Jean-Michel Lavarenne <jm@jupiterweb.ca>
2015-12-11 15:59:34 -0500

    Start work on RSS page and pathauto for RSS page.

--- docroot/sites/all/themes/shelter/templates/pages/page.tpl.php
+++ docroot/sites/all/themes/shelter/templates/pages/page.tpl.php
@@ -74,7 +74,7 @@
     <?php print partial('homepage', array('page' => $page, 'hot_responses' => $hot_responses, 'upcoming_events' => $upcoming_events)); ?>
 
   <?php elseif ($global_events_page): ?>
-    <?php print partial('homepage', array('page' => $page, 'hot_responses' => $hot_responses)); ?>
+    <?php print partial('homepage', array('page' => $page, 'hot_responses' => $hot_responses, 'upcoming_events' => FALSE)); ?>
 
   <?php elseif ($is_regions_and_countries): ?>
     <?php print partial('regions', array('page' => $page)); ?>
