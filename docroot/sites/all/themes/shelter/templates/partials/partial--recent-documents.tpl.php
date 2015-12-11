f32772e2be6641d0d3f43874550fc7d7375d0514
Jerome Tchania <jerome@appnovation.com>
2015-12-11 15:49:49 -0500

    Add a function to get 5 most recent documents

---
+++ docroot/sites/all/themes/shelter/templates/partials/partial--recent-documents.tpl.php
@@ -0,0 +1,17 @@
+<?php // dpm($variables); ?>
+
+<section id="shelter-documents">
+  <div id="box-documents">
+    <?php foreach($documents as $document): ?>
+      <div class="event-card">
+        <div class="event-title">
+          <span class="arrow">›</span>
+          <?php print $document['title']; ?>
+        </div>
+    <?php endforeach; ?>
+
+    <div class="all-documents">
+      <?php print $all_documents_link; ?>
+    </div>
+  </div>
+</section>
