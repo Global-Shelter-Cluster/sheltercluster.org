b8b127a852c31e4a00055941ebdac4e3e42593a4
Antoine Lafontaine <antoine@appnovation.com>
2015-12-14 00:07:18 -0500

    Better full events list markup and styles

--- docroot/sites/all/themes/shelter/templates/partials/partial--all-events.tpl.php
+++ docroot/sites/all/themes/shelter/templates/partials/partial--all-events.tpl.php
@@ -1,40 +1,55 @@
-<section id="shelter-calendar">
-  <div id="box-calendar">
-    <?php print _svg('icons/pin', array('id' => 'calendar-pin-icon', 'alt' => t('An icon representing a calendar with a pin on it.'))); ?>
+<section id="all-events">
+
     <div id="date-calendar"><?php print $title ?></div>
+
     <?php foreach($events as $event): ?>
-      <div class="event-card">
+      <?php if($event['is_future']): ?>
+        <div class="event-card future-event-card">
+      <?php else: ?>
+        <div class="event-card past-event-card">
+      <?php endif; ?>
         <div class="event-title">
           <span class="arrow">›</span>
           <?php print $event['link']; ?>
         </div>
+
+        <?php if($event['is_future']): ?>
+          <div class="table-wrapper">
+            <div class="event-information">
+        <?php endif; ?>
+
         <div class="information-item event-date">
           <span class="label"><?php print t('Event date: '); ?></span>
           <span><?php print $event['date']; ?></span>
-          <?php if($event['is_future']): ?>
-            <div style="text-decoration: blink; color: red;">This event is in the future.</div>
-          <?php endif; ?>
-          <?php if($event['is_past']): ?>
-            <div style="text-decoration: blink; color: red;">This event is in the past.</div>
-          <?php endif; ?>
-        </div>
-        <div class="information-item event-location">
-          <span class="label"><?php print t('Location: '); ?></span>
-          <span>
-            <?php print render($event['location']); ?>
-            &nbsp;[<?php print $event['map_link']; ?>]
-          </span>
-        </div>
-        <div class="information-item event-contact">
-          <span class="label"><?php print t('Contact: '); ?></span>
-          <span><?php print $event['contact']; ?></span>
         </div>
 
-        <div class="event-map">
-          <?php print $event['static_map']; ?>
-        </div>
+        <?php if($event['location']): ?>
+          <div class="information-item event-location">
+            <span class="label"><?php print t('Location: '); ?></span>
+            <span>
+              <?php print render($event['location']); ?>
+            </span>
+          </div>
+        <?php endif; ?>
+
+        <?php if($event['contact']): ?>
+          <div class="information-item event-contact">
+            <span class="label"><?php print t('Contact: '); ?></span>
+            <span><?php print $event['contact']; ?></span>
+          </div>
+        <?php endif; ?>
+
+        <?php if($event['is_future']): ?>
+            </div>
+            <div class="event-map">
+              <div>
+                <?php print $event['static_map']; ?>
+              </div>
+            </div>
+          </div>
+        <?php endif; ?>
+
       </div>
     <?php endforeach; ?>
 
-  </div>
 </section>
--- docroot/sites/all/themes/shelter/templates/partials/partial--upcoming-events.tpl.php
+++ docroot/sites/all/themes/shelter/templates/partials/partial--upcoming-events.tpl.php
@@ -1,5 +1,3 @@
-<?php // dpm($variables); ?>
-
 <section id="shelter-calendar">
   <div id="box-calendar">
     <?php print _svg('icons/pin', array('id' => 'calendar-pin-icon', 'alt' => t('An icon representing a calendar with a pin on it.'))); ?>
@@ -14,21 +12,26 @@
           <span class="label"><?php print t('Event date: '); ?></span>
           <span><?php print $event['date']; ?></span>
         </div>
-        <div class="information-item event-location">
-          <span class="label"><?php print t('Location: '); ?></span>
-          <span>
-            <?php print render($event['location']); ?>
-            &nbsp;[<?php print $event['map_link']; ?>]
-          </span>
-        </div>
-        <div class="information-item event-contact">
-          <span class="label"><?php print t('Contact: '); ?></span>
-          <span><?php print $event['contact']; ?></span>
-        </div>
 
-        <div class="event-map">
-          <?php print $event['static_map']; ?>
-        </div>
+        <?php if($event['location']): ?>
+          <div class="information-item event-location">
+            <span class="label">
+              <?php print t('Location: '); ?>
+              <span class="map-link"><?php print $event['map_link']; ?></span>
+            </span>
+            <span>
+              <?php print render($event['location']); ?>
+            </span>
+          </div>
+        <?php endif; ?>
+
+        <?php if($event['contact']): ?>
+          <div class="information-item event-contact">
+            <span class="label"><?php print t('Contact: '); ?></span>
+            <span><?php print $event['contact']; ?></span>
+          </div>
+        <?php endif; ?>
+
       </div>
     <?php endforeach; ?>
 
