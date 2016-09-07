3734ba1a902009ab8c2b206961f258a9a84d4415
Rob Wild <robert@appnovation.com>
2016-09-07 11:51:35 +0100

    Fix for https://appnovation.atlassian.net/browse/RDC-60 'Long document title overlaps with Events block' added js logic to truncate if strong is long enough, worked more efficiently than a CSS option

---
+++ docroot/sites/all/themes/shelter/assets/javascripts/truncating-titles.js
@@ -0,0 +1,18 @@
+(function ($) {
+  Drupal.behaviors.truncatingtitles = {
+    attach: function (context, settings) {
+       console.log('hello truncation');
+
+        $('.document-row .information-title a').each(function() {
+            var title = $.trim($(this).text());
+            var max = 50;
+
+            if (title.length > max) {
+                var shortText = jQuery.trim(title).substring(0, max - 3) + '...';
+                $(this).html('<span>' + shortText + '</span>');
+            }
+        });
+  
+    }
+  };
+})(jQuery);
