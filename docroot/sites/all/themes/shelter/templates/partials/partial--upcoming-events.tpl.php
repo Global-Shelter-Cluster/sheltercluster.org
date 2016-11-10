97bab9c45a0e286b96ae35449f805a3121374bb2
Oliver Davies <oliver@appnovation.com>
2016-11-10 16:40:53 +0000

    Recent documents and upcoming events styling
    RDC-93

--- docroot/sites/all/themes/shelter/templates/partials/partial--upcoming-events.tpl.php
+++ docroot/sites/all/themes/shelter/templates/partials/partial--upcoming-events.tpl.php
@@ -1,64 +1,43 @@
-<section id="shelter-calendar">
+<section id="shelter-calendar" class="upcoming-events">
   <div id="box-calendar">
-    <?php print _svg('icons/pin', array('id' => 'calendar-pin-icon', 'alt' => t('An icon representing a calendar with a pin on it.'))); ?>
-    <div id="date-calendar"><?php print $title ?></div>
-    <?php foreach($events as $event): ?>
-      <div class="event-card">
+    <div id="date-calendar">
+      <?php print $title ?>
+    </div>
+
+    <?php foreach ($events as $key => $event): ?>
+      <div class="event-card<?php print ($key === 0) ? ' is-first' : ''; ?>">
         <div class="event-title">
-          <span class="arrow">›</span>
           <?php print $event['link']; ?>
         </div>
-        <div class="information-item event-date">
-          <span class="label"><?php print t('Event date: '); ?></span>
-          <span><?php print render($event['date']); ?></span>
+
+        <div class="response is-small">
+          <?php print $event['response']; ?>
         </div>
 
-        <?php if($event['location']): ?>
-          <div class="information-item event-location">
+        <?php if ($event['location']): ?>
+          <div class="information-item event-location is-small">
             <span class="label">
               <?php print t('Location: '); ?>
-              <span class="map-link"><?php print $event['map_link']; ?></span>
             </span>
+
             <span>
               <?php print render($event['location']); ?>
             </span>
           </div>
         <?php endif; ?>
 
-        <?php if($event['contact']): ?>
-          <div class="information-item event-contact">
-            <span class="label"><?php print t('Contact: '); ?></span>
-            <span><?php print $event['contact']; ?></span>
-          </div>
-        <?php endif; ?>
-        
-        <div class="response">
-          <?php print $event['response']; ?>
+        <div class="information-item event-date is-small">
+          <span><?php print render($event['date']); ?></span>
         </div>
-        
+  
         <div class="ical">
           <?php print $event['ical']; ?>
-        </div>        
-
+        </div><!-- /.ical -->
       </div>
     <?php endforeach; ?>
 
     <div class="all-events">
       <?php print $all_events_link; ?>
-    </div>
-
-    <?php if ($global_events_link): ?>
-      <div class="all-events">
-        <?php print render($global_events_link); ?>
-      </div>
-    <?php endif; ?>
-
-    <?php if ($previous_events_link): ?>
-      <div class="previous-events">
-        <a href="<?php print $previous_events_link; ?>">
-          <?php print t('View Previous Events'); ?>
-        </a>
-      </div>
-    <?php endif; ?>
+    </div><!-- /.all-events -->
   </div>
 </section>
