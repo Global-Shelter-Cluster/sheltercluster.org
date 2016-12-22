7d2a779e101f3c81977bde6c52681383f94e4738
Oliver Davies <oliver@appnovation.com>
2016-12-22 09:41:27 +0000

    Add "document contains" search box
    RDC-82

--- docroot/sites/all/modules/custom/cluster_docs/js/search.js
+++ docroot/sites/all/modules/custom/cluster_docs/js/search.js
@@ -2,15 +2,17 @@
 
   Drupal.behaviors.cluster_docs_search = {
     attach: function (context, settings) {
-      $('.facet input[name=title]').once('cluster_docs_search').change(function() {
-        $(this).closest('.facet').find('input[name=title_search]').click();
-      });
-      $('.facet input[name=title_search]').once('cluster_docs_search').click(function() {
+      $('.facet input[name=title]').once('cluster_docs_search').change(function () {
+            $(this).closest('.facet').find('input[name=title_search]').click();
+        });
+
+      $('.facet input[name=title_search]').once('cluster_docs_search').click(function () {
         var value = $(this).closest('.facet').find('input[name=title]').val();
         var href = window.location.href;
 
         // Remove title parameter from current URL, if any
         href = href.replace(/\?f\[\d*\]=title%3A[^&]*&/, '?').replace(/&f\[\d*\]=title%3A[^&]*/, '').replace(/\?f\[\d*\]=title%3A[^&]*$/, '');
+        href = href.replace(/\?f\[\d*\]=attachments_field_file%3A[^&]*&/, '?').replace(/&f\[\d*\]=attachments_field_file%3A[^&]*/, '').replace(/\?f\[\d*\]=attachments_field_file%3A[^&]*$/, '');
 
         if (value) {
           var connector = href.indexOf('?') == -1 ? '?' : '&';
@@ -19,7 +21,21 @@
 
         window.location.href = href;
       });
+
+      $('.facet input[name=document_search]').once('cluster_docs_search').click(function () {
+        var value = $(this).closest('.facet').find('input[name=document]').val();
+        var href = window.location.href;
+
+        href = href.replace(/\?f\[\d*\]=title%3A[^&]*&/, '?').replace(/&f\[\d*\]=title%3A[^&]*/, '').replace(/\?f\[\d*\]=title%3A[^&]*$/, '');
+        href = href.replace(/\?f\[\d*\]=attachments_field_file%3A[^&]*&/, '?').replace(/&f\[\d*\]=attachments_field_file%3A[^&]*/, '').replace(/\?f\[\d*\]=attachments_field_file%3A[^&]*$/, '');
+
+        if (value) {
+          var connector = href.indexOf('?') == -1 ? '?' : '&';
+          href = href + connector + 'f[]=attachments_field_file%3A' + encodeURIComponent(value);
+        }
+
+        window.location.href = href;
+      });
     }
   }
-
 })(jQuery);
