4370d140fbb2ed8ff600ddb219572741666580d0
Camilo Bravo <hola@cambraca.com>
2017-11-10 16:30:32 -0500

    SCW-30 Search filter by index (e.g. ”only show events”)

--- docroot/sites/all/modules/custom/cluster_search/js/algolia.js
+++ docroot/sites/all/modules/custom/cluster_search/js/algolia.js
@@ -129,13 +129,13 @@
             timeout: null,
             results: null,
             searching: false,
-            shouldScrollOnResults: true
+            shouldScrollOnResults: true,
+            indexFilter: null
           },
           computed: {
             hasResults: function() {
               var ret = this.results && (
                 this.results.documents.length > 0 ||
-                this.results.documents2.length > 0 ||
                 this.results.events.length > 0 ||
                 this.results.pages.length > 0 ||
                 this.results.groups.length > 0 ||
@@ -157,17 +157,53 @@
                 clearTimeout(this.timeout);
               this.searching = true;
               this.timeout = setTimeout(this.search, 150);
+            },
+            indexFilter: function(value) {
+              if (!value)
+                this.limitResults();
+              this.search();
+              this.focus();
             }
           },
           methods: {
+            limitResults: function() {
+              if (!this.results)
+                return;
+
+              if (this.results.documents)
+                this.results.documents = this.getPage(this.results.documents, 0, 20);
+              if (this.results.events)
+                this.results.events = this.getPage(this.results.events, 0, 10);
+              if (this.results.pages)
+                this.results.pages = this.getPage(this.results.pages, 0, 8);
+              if (this.results.groups)
+                this.results.groups = this.getPage(this.results.groups, 0, 8);
+              if (this.results.contacts)
+                this.results.contacts = this.getPage(this.results.contacts, 0, 6);
+
+              // Limit "pages + groups" to a maximum of 8 results
+              if (this.results.pages && this.results.groups) {
+                this.results.pages = this.results.pages.slice(0, 8 - Math.min(this.results.groups.length, 4));
+                this.results.groups = this.results.groups.slice(0, 8 - Math.min(this.results.pages.length, 4));
+              }
+            },
+            getPage: function(items, page, items_per_page) {
+              return items.slice(items_per_page * page, items_per_page * page + items_per_page);
+            },
             focusHandler: function() {
               $('#cluster-search-mega-menu').addClass('force-visible').parent().addClass('disable-hover');
+              if (this.hasResults)
+                this.scroll();
             },
             blurHandler: function() {
               $('#cluster-search-mega-menu').removeClass('force-visible').parent().removeClass('disable-hover');
             },
             focus: function() {
               $('#cluster-search-mega-menu input').focus();
+            },
+            scroll: function() {
+              $('html, body').animate({scrollTop: $('#cluster-search-mega-menu').offset().top}, 400);
+              this.shouldScrollOnResults = false;
             },
             search: function() {
               var vue = this;
@@ -187,7 +223,7 @@
                 indexName: algolia_prefix + 'Documents',
                 query: vue.query,
                 params: {
-                  hitsPerPage: 20
+                  hitsPerPage: !vue.indexFilter ? 20 : (vue.indexFilter === 'documents' ? 50 : 0)
                 }
               });
 
@@ -195,7 +231,7 @@
                 indexName: algolia_prefix + 'Events',
                 query: vue.query,
                 params: {
-                  hitsPerPage: 10
+                  hitsPerPage: !vue.indexFilter ? 10 : (vue.indexFilter === 'events' ? 50 : 0)
                 }
               });
 
@@ -203,7 +239,7 @@
                 indexName: algolia_prefix + 'Pages',
                 query: vue.query,
                 params: {
-                  hitsPerPage: 10
+                  hitsPerPage: !vue.indexFilter ? 8 : (vue.indexFilter === 'pages' ? 40 : 0)
                 }
               });
 
@@ -211,7 +247,7 @@
                 indexName: algolia_prefix + 'Groups',
                 query: vue.query,
                 params: {
-                  hitsPerPage: 10
+                  hitsPerPage: !vue.indexFilter ? 8 : (vue.indexFilter === 'groups' ? 40 : 0)
                 }
               });
 
@@ -219,7 +255,7 @@
                 indexName: algolia_prefix + 'Contacts',
                 query: vue.query,
                 params: {
-                  hitsPerPage: 6
+                  hitsPerPage: !vue.indexFilter ? 6 : (vue.indexFilter === 'contacts' ? 30 : 0)
                 }
               });
 
@@ -231,7 +267,6 @@
 
                 var ret = {
                   documents: [],
-                  documents2: [],
                   events: [],
                   pages: [],
                   groups: [],
@@ -239,9 +274,7 @@
                 };
 
                 if (content.results[0].hits.length > 0)
-                  ret.documents = content.results[0].hits.slice(0, 10).map(processDocument);
-                if (content.results[0].hits.length > 10)
-                  ret.documents2 = content.results[0].hits.slice(10).map(processDocument);
+                  ret.documents = content.results[0].hits.map(processDocument);
                 if (content.results[1].hits.length > 0)
                   ret.events = content.results[1].hits.map(processEvent);
                 if (content.results[2].hits.length > 0)
@@ -251,16 +284,12 @@
                 if (content.results[4].hits.length > 0)
                   ret.contacts = content.results[4].hits.map(processContact);
 
-                // Limit "pages + groups" to a maximum of 8 results
-                ret.pages = ret.pages.slice(0, 8 - Math.min(ret.groups.length, 4));
-                ret.groups = ret.groups.slice(0, 8 - Math.min(ret.pages.length, 4));
-
                 vue.results = ret;
+                if (!vue.indexFilter)
+                  vue.limitResults();
                 vue.searching = false;
-                if (vue.shouldScrollOnResults && vue.hasResults) {
-                  $('html, body').animate({scrollTop: $('#cluster-search-mega-menu').offset().top}, 400);
-                  vue.shouldScrollOnResults = false;
-                }
+                if (vue.shouldScrollOnResults && vue.hasResults)
+                  vue.scroll();
               });
             }
           }
--- docroot/sites/all/modules/custom/cluster_search/templates/cluster_search_mega_menu.tpl.php
+++ docroot/sites/all/modules/custom/cluster_search/templates/cluster_search_mega_menu.tpl.php
@@ -2,55 +2,67 @@
   <a href="#" @click.prevent="focus">Search</a>
   <ul class="nav-items menu search-input-row">
     <li class="search-input">
-        <input type="search" v-model="query" @focus="focusHandler" @blur="blurHandler">
+      <input type="search" v-model="query" @focus="focusHandler"
+             @blur="blurHandler">
     </li>
     <li class="algolia-logo">
-        <img src="https://www.algolia.com/static_assets/images/press/downloads/search-by-algolia.png" alt="Search by Algolia">
+      <img
+        src="https://www.algolia.com/static_assets/images/press/downloads/search-by-algolia.png"
+        alt="Search by Algolia">
     </li>
   </ul>
-  <ul class="nav-items menu" v-if="hasResults">
+  <ul class="nav-items menu" v-if="hasResults && !indexFilter">
     <li v-if="results.documents.length > 0">
-      <a>Documents</a>
+      <a href="#" @click.prevent="indexFilter = 'documents'">Documents</a>
       <ul class="nav-items menu">
-        <li class="leaf" v-for="document in results.documents">
-          <a :href="document.url" class="search-result-title" :title="document.title">
-              {{ document.title }}
-              <small v-if="document.group" class="search-result-title" :title="document.group">{{ document.group }}</small>
-              <small>
-                  {{ document.date }}
-                  <template v-if="document.featured">&middot; Featured</template>
-                  <template v-if="document.key">&middot; Key</template>
-              </small>
+        <li class="leaf" v-for="document in getPage(results.documents, 0, 10)">
+          <a :href="document.url" class="search-result-title"
+             :title="document.title">
+            {{ document.title }}
+            <small v-if="document.group" class="search-result-title"
+                   :title="document.group">{{ document.group }}
+            </small>
+            <small>
+              {{ document.date }}
+              <template v-if="document.featured">&middot; Featured</template>
+              <template v-if="document.key">&middot; Key</template>
+            </small>
           </a>
         </li>
       </ul>
     </li>
-    <li v-if="results.documents2.length > 0">
-      <a>&nbsp;</a>
+    <li v-if="results.documents.length > 10">
+      <div>&nbsp;</div>
       <ul class="nav-items menu">
-        <li class="leaf" v-for="document in results.documents2">
-          <a :href="document.url" class="search-result-title" :title="document.title">
-              {{ document.title }}
-              <small v-if="document.group" class="search-result-title" :title="document.group">{{ document.group }}</small>
-              <small>
-                  <template v-if="document.date">{{ document.date }}</template>
-                  <template v-if="document.featured">&middot; Featured</template>
-                  <template v-if="document.key">&middot; Key</template>
-              </small>
+        <li class="leaf" v-for="document in getPage(results.documents, 1, 10)">
+          <a :href="document.url" class="search-result-title"
+             :title="document.title">
+            {{ document.title }}
+            <small v-if="document.group" class="search-result-title"
+                   :title="document.group">{{ document.group }}
+            </small>
+            <small>
+              <template v-if="document.date">{{ document.date }}</template>
+              <template v-if="document.featured">&middot; Featured</template>
+              <template v-if="document.key">&middot; Key</template>
+            </small>
           </a>
         </li>
       </ul>
     </li>
     <li v-if="results.events.length > 0">
-      <a>Events</a>
+      <a href="#" @click.prevent="indexFilter = 'events'">Events</a>
       <ul class="nav-items menu">
         <li class="leaf" v-for="event in results.events">
           <a :href="event.url" class="search-result-title" :title="event.title">
             {{ event.title }}
-            <small v-if="event.group" class="search-result-title" :title="event.group">{{ event.group }}</small>
+            <small v-if="event.group" class="search-result-title"
+                   :title="event.group">{{ event.group }}
+            </small>
             <small>
               {{ event.date }}
-              <template v-if="event.location">&middot; {{ event.location }}</template>
+              <template v-if="event.location">&middot; {{ event.location }}
+              </template>
             </small>
           </a>
         </li>
@@ -58,56 +70,180 @@
     </li>
     <li v-if="results.pages.length > 0 || results.groups.length > 0">
       <template v-if="results.pages.length > 0">
-      <a>Pages</a>
+        <a href="#" @click.prevent="indexFilter = 'pages'">Pages</a>
         <ul class="nav-items menu">
           <li class="leaf" v-for="page in results.pages">
             <a :href="page.url">
-                {{ page.title }}
-                <small v-if="page.group" class="search-result-title" :title="page.group">{{ page.group }}</small>
-                <small>{{ page.type }}</small>
+              {{ page.title }}
+              <small v-if="page.group" class="search-result-title"
+                     :title="page.group">{{ page.group }}
+              </small>
+              <small>{{ page.type }}</small>
             </a>
           </li>
         </ul>
       </template>
       <br v-if="results.pages.length > 0 && results.groups.length > 0">
       <template v-if="results.groups.length > 0">
-        <a>Groups</a>
+        <a href="#" @click.prevent="indexFilter = 'groups'">Groups</a>
         <ul class="nav-items menu">
           <li class="leaf" v-for="group in results.groups">
             <a :href="group.url">
-                {{ group.title }}
-                <small>{{ group.type }}</small>
+              {{ group.title }}
+              <small>{{ group.type }}</small>
             </a>
           </li>
         </ul>
       </template>
     </li>
     <li v-if="results.contacts.length > 0">
-      <a>Contacts</a>
+      <a href="#" @click.prevent="indexFilter = 'contacts'">Contacts</a>
       <ul class="nav-items menu">
         <li class="leaf" v-for="contact in results.contacts">
           <a :href="contact.url" target="_blank">
-              {{ contact.title }}
-              <small v-if="contact.group" class="search-result-title" :title="contact.group">{{ contact.group }}</small>
-              <small v-if="contact.role || contact.org">
-                  <template v-if="contact.org">
-                      {{ contact.org}}
-                      <template v-if="contact.role">
-                          &middot; {{ contact.role }}
-                      </template>
-                  </template>
-                  <template v-if="!contact.org && contact.role">
-                      {{ contact.role }}
-                  </template>
-              </small>
-              <small v-if="contact.email">{{ contact.email }}</small>
-              <small v-if="contact.phone">{{ contact.phone }}</small>
+            {{ contact.title }}
+            <small v-if="contact.group" class="search-result-title"
+                   :title="contact.group">{{ contact.group }}
+            </small>
+            <small v-if="contact.role || contact.org">
+              <template v-if="contact.org">
+                {{ contact.org}}
+                <template v-if="contact.role">
+                  &middot; {{ contact.role }}
+                </template>
+              </template>
+              <template v-if="!contact.org && contact.role">
+                {{ contact.role }}
+              </template>
+            </small>
+            <small v-if="contact.email">{{ contact.email }}</small>
+            <small v-if="contact.phone">{{ contact.phone }}</small>
           </a>
         </li>
       </ul>
     </li>
   </ul>
-  <ul class="nav-items menu" v-if="showNoResultsMessage">
-      <li><a>No results found for "{{ query }}"</a></li>
+  <ul class="nav-items menu" v-if="hasResults && indexFilter == 'documents'">
+    <template v-for="page in 5">
+      <li v-if="results.documents.length > (page-1) * 10">
+        <a v-if="page === 1" href="#" @click.prevent="indexFilter = null">Documents</a>
+        <div v-if="page > 1">&nbsp;</div>
+        <ul class="nav-items menu">
+          <li class="leaf"
+              v-for="document in getPage(results.documents, page-1, 10)">
+            <a :href="document.url" class="search-result-title"
+               :title="document.title">
+              {{ document.title }}
+              <small v-if="document.group" class="search-result-title"
+                     :title="document.group">{{ document.group }}
+              </small>
+              <small>
+                {{ document.date }}
+                <template v-if="document.featured">&middot; Featured</template>
+                <template v-if="document.key">&middot; Key</template>
+              </small>
+            </a>
+          </li>
+        </ul>
+      </li>
+    </template>
+  </ul>
+  <ul class="nav-items menu" v-if="hasResults && indexFilter == 'events'">
+    <template v-for="page in 5">
+      <li v-if="results.events.length > (page-1) * 10">
+        <a v-if="page === 1" href="#" @click.prevent="indexFilter = null">Events</a>
+        <div v-if="page > 1">&nbsp;</div>
+        <ul class="nav-items menu">
+          <li class="leaf" v-for="event in getPage(results.events, page-1, 10)">
+            <a :href="event.url" class="search-result-title" :title="event.title">
+              {{ event.title }}
+              <small v-if="event.group" class="search-result-title"
+                     :title="event.group">{{ event.group }}
+              </small>
+              <small>
+                {{ event.date }}
+                <template v-if="event.location">&middot; {{ event.location }}
+                </template>
+              </small>
+            </a>
+          </li>
+        </ul>
+      </li>
+    </template>
+  </ul>
+  <ul class="nav-items menu" v-if="hasResults && indexFilter == 'pages'">
+    <template v-for="page in 5">
+      <li v-if="results.pages.length > (page-1) * 8">
+        <a v-if="page === 1" href="#" @click.prevent="indexFilter = null">Pages</a>
+        <div v-if="page > 1">&nbsp;</div>
+        <ul class="nav-items menu">
+          <li class="leaf"
+              v-for="page in getPage(results.pages, page-1, 8)">
+            <a :href="page.url">
+              {{ page.title }}
+              <small v-if="page.group" class="search-result-title"
+                     :title="page.group">{{ page.group }}
+              </small>
+              <small>{{ page.type }}</small>
+            </a>
+          </li>
+        </ul>
+      </li>
+    </template>
+  </ul>
+  <ul class="nav-items menu" v-if="hasResults && indexFilter == 'groups'">
+    <template v-for="page in 5">
+      <li v-if="results.groups.length > (page-1) * 8">
+        <a v-if="page === 1" href="#" @click.prevent="indexFilter = null">Groups</a>
+        <div v-if="page > 1">&nbsp;</div>
+        <ul class="nav-items menu">
+          <li class="leaf"
+              v-for="group in getPage(results.groups, page-1, 8)">
+            <a :href="group.url">
+              {{ group.title }}
+              <small>{{ group.type }}</small>
+            </a>
+          </li>
+        </ul>
+      </li>
+    </template>
+  </ul>
+  <ul class="nav-items menu" v-if="hasResults && indexFilter == 'contacts'">
+    <template v-for="page in 5">
+      <li v-if="results.contacts.length > (page-1) * 6">
+        <a v-if="page === 1" href="#" @click.prevent="indexFilter = null">Contacts</a>
+        <div v-if="page > 1">&nbsp;</div>
+        <ul class="nav-items menu">
+          <li class="leaf"
+              v-for="contact in getPage(results.contacts, page-1, 6)">
+            <a :href="contact.url" target="_blank">
+              {{ contact.title }}
+              <small v-if="contact.group" class="search-result-title"
+                     :title="contact.group">{{ contact.group }}
+              </small>
+              <small v-if="contact.role || contact.org">
+                <template v-if="contact.org">
+                  {{ contact.org}}
+                  <template v-if="contact.role">
+                    &middot; {{ contact.role }}
+                  </template>
+                </template>
+                <template v-if="!contact.org && contact.role">
+                  {{ contact.role }}
+                </template>
+              </small>
+              <small v-if="contact.email">{{ contact.email }}</small>
+              <small v-if="contact.phone">{{ contact.phone }}</small>
+            </a>
+          </li>
+        </ul>
+      </li>
+    </template>
+  </ul>
+  <ul class="nav-items menu search-no-results" v-if="showNoResultsMessage">
+    <li>
+      <div>No results found for "{{ query }}"</div>
+      <a v-if="indexFilter" href="#" @click.prevent="indexFilter = null">Search everything</a>
+    </li>
   </ul>
 </li>
