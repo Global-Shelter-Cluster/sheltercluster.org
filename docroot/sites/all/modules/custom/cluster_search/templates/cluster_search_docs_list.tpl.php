492dd7dc874b8c8c96e262802575862220451448
Camilo Bravo <hola@cambraca.com>
2017-11-24 20:26:36 -0500

    SCW-31 Initial styling for document previews

--- docroot/sites/all/modules/custom/cluster_search/templates/cluster_search_docs_list.tpl.php
+++ docroot/sites/all/modules/custom/cluster_search/templates/cluster_search_docs_list.tpl.php
@@ -1,16 +1,27 @@
-<section class="document-list">
+<section class="document-preview-list">
 
-  <a v-for="document in results" :href="document.url" :title="document.title|strip_tags">
-    <h4 v-html="document.title"></h4>
-    <small v-if="includeDescendants == 1" v-html="document.group"></small>
-    <img v-if="document.thumb" :src="document.thumb">
+  <a class="document-preview" v-for="document in results" :href="document.url">
+    <img class="thumbnail" v-if="document.thumb" :src="document.thumb">
     <div v-if="!document.thumb">
-
+      ?
     </div>
+    <h4 :title="document.title|strip_tags" v-html="
+    (
+      document.featured && document.key
+      ? '<span title=\'Featured and key document\'>★</span> '
+      : (
+        document.featured
+        ? '<span title=\'Featured\'>★</span> '
+        : (
+          document.key
+          ? '<span title=\'Key document\'>★</span> '
+          : ''
+        )
+      )
+    ) + document.title"></h4>
+    <small v-if="includeDescendants == 1" v-html="document.group"></small>
     <small>
       {{ document.date }}
-      <template v-if="document.featured">&middot; Featured</template>
-      <template v-if="document.key">&middot; Key document</template>
     </small>
   </a>
 
