64c7b235a183c2421a1b48fb78b822cf99c8f2fb
Camilo Bravo <hola@cambraca.com>
2017-12-01 13:30:35 -0500

    SCW-31 doc preview display

--- docroot/sites/all/modules/custom/cluster_search/templates/cluster_search_docs_list.tpl.php
+++ docroot/sites/all/modules/custom/cluster_search/templates/cluster_search_docs_list.tpl.php
@@ -1,11 +1,11 @@
 <section class="document-preview-list">
 
   <article class="document-preview" v-for="document in results">
-    <a :href="document.url">
-      <img class="thumbnail" v-if="document.thumb" :src="document.thumb">
-      <div v-if="!document.thumb">
-        ?
-      </div>
+    <a v-if="document.thumb" :href="document.url">
+      <img class="thumbnail" :src="document.thumb">
+<!--      <div v-if="!document.thumb">-->
+<!--        ?-->
+<!--      </div>-->
     </a>
     <a :href="document.url">
       <h4 :title="document.title|strip_tags" v-html="
@@ -30,30 +30,23 @@
        v-html="document.group">
     </a>
 
-
-    <div v-if="document.field_document_source" class="document-source is-small">
-      Source: {{ document.field_document_source }}
+    <div v-if="document.date" class="document-date">
+      {{ document.date }}
+      <span v-if="document.field_document_source" :title="'Source: ' + document.field_document_source">
+        <span v-if="document.date">&middot;</span>
+        Source: {{ document.field_document_source }}
+      </span>
     </div>
 
     <div class="file-info">
-      <span v-if="document['field_file:file:url']" class="file-extension is-small">
-        [ {{ document['field_file:file:size']|file_size }} ]
-        {{ document['field_file:file:url']|file_extension }}
-      </span>
+      <span>[ {{ document['field_file:file:size']|file_size }} ]</span>
+      <span v-if="document['field_file:file:url']"
+            class="file-extension"
+      >{{ document['field_file:file:url']|file_extension }}</span>
       <span class="document-link">
-        <a :href="document['field_file:file:url']">Download</a>
+        <a :href="document['field_file:file:url']" target="_blank">Download</a>
       </span>
     </div>
-
-    <div class="document-date is-small">{{ document.date }}</div>
-
-<!--    <div class="info">-->
-<!--      {{ document.date }}-->
-<!--      <span class="file-extension is-small">{{ 'a.pdf'|file_extension }}</span>-->
-<!--      <span class="document-link">-->
-<!--        <a href="http://local.sheltercluster.org/haiti-hurricane-matthew-2016/documents/usaid-ofda-report-wind-testing-small-transitional-housing">Download</a>-->
-<!--      </span>-->
-<!--    </div>-->
   </article>
 
 </section>
