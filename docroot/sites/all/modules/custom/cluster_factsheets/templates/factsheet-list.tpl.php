128d8acdb37071701fc50685f7632356201c9f40
Camilo Bravo <hola@cambraca.com>
2018-04-04 10:24:15 -0500

    SCW-83 Progress on factsheets

--- docroot/sites/all/modules/custom/cluster_factsheets/templates/factsheet-list.tpl.php
+++ docroot/sites/all/modules/custom/cluster_factsheets/templates/factsheet-list.tpl.php
@@ -1,25 +1,6 @@
 <section class="factsheet-preview-list">
-  <?php foreach ($items as $item): ?>
-    <article class="preview-block factsheet-preview">
-      <a href="<?php print $item['link']; ?>" class="thumbnail">
-        <img src="<?php print $item['map']; ?>">
-        <img src="<?php print $item['image']; ?>">
-      </a>
-      <a href="<?php print $item['link']; ?>">
-        <h4><?php print date('j M Y', $item['date']); ?></h4>
-      </a>
-
-      <div class="summary">
-        <?php print $item['summary']; ?>
-      </div>
-      <ul class="indicators">
-        <?php foreach ($item['indicators'] as $indicator => $value): ?>
-        <li>
-          <strong><?php print $value; ?></strong>
-          <?php print $indicator; ?>
-        </li>
-        <?php endforeach; ?>
-      </ul>
-    </article>
-  <?php endforeach; ?>
+  <?php
+  foreach ($items as $item)
+    print render($item);
+  ?>
 </section>
---
+++ docroot/sites/all/themes/shelter/templates/nodes/node--factsheet--teaser.tpl.php
@@ -0,0 +1,21 @@
+<article class="preview-block factsheet-preview">
+  <a href="<?php print url('node/' . $nid); ?>" class="thumbnail">
+    <?php print render($content['field_image']); ?>
+    <?php print render($content['field_map']); ?>
+  </a>
+  <a href="<?php print url('node/' . $nid); ?>">
+    <h4><?php print render($content['field_date']); ?></h4>
+  </a>
+
+  <div class="summary">
+    <?php print render($content['body']); ?>
+  </div>
+  <ul class="indicators">
+    <?php foreach ($content['indicators'] as $indicator => $value): ?>
+      <li>
+        <strong><?php print $value; ?></strong>
+        <?php print $indicator; ?>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+</article>
