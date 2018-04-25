84719ef451bc72a03ca2364b96dab3885607b803
Camilo Bravo <hola@cambraca.com>
2018-04-25 17:34:37 -0500

    SCW-83 Figures represent HH or individual people

--- docroot/sites/all/themes/shelter/templates/field/field--factsheet.tpl.php
+++ docroot/sites/all/themes/shelter/templates/field/field--factsheet.tpl.php
@@ -1,6 +1,10 @@
 <div>
   <?php if (!$label_hidden): ?>
-    <h3><?php print $label ?></h3>
+    <h3><?php
+      print $label;
+      if (isset($cluster_factsheets) && isset($cluster_factsheets['hh_abbr']))
+        print $cluster_factsheets['hh_abbr'];
+      ?></h3>
   <?php endif; ?>
   <?php
   foreach ($items as $delta => $item)
