35d1d6d412a04090aaa0c84d338ac0307a493a68
Camilo Bravo <hola@cambraca.com>
2018-11-26 15:53:30 +0100

    SA-6 Exposing webforms to API

---
+++ docroot/sites/all/modules/custom/cluster_api/classes/ClusterAPI_Type_Webform.php
@@ -0,0 +1,26 @@
+<?php
+
+class ClusterAPI_Type_Webform extends ClusterAPI_Type {
+
+  protected static $type = 'webform';
+  protected static $related_def = [
+    'groups' => [
+      'type' => 'group',
+      'mode' => ClusterAPI_Object::MODE_STUB,
+    ],
+  ];
+
+  protected function generateObject($id, $mode) {
+    $node = node_load($id);
+    if ($node->type !== 'webform')
+      return NULL;
+
+//    $wrapper = entity_metadata_wrapper('node', $node);
+
+    return [
+      'groups' => self::getReferenceIds('node', $node, 'og_group_ref', TRUE),
+      'title' => $node->title,
+      'fields' => cluster_webform_export($node),
+    ];
+  }
+}
