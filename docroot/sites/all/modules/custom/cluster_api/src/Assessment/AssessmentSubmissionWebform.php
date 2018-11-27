689f4dab7cbe69be1a89e7da4025216ea17cd529
Jean-Michel Lavarenne <jmlavarenne@gmail.com>
2018-11-27 16:22:11 -0500

    Add new service skeleton to handle post submissions.

---
+++ docroot/sites/all/modules/custom/cluster_api/src/Assessment/AssessmentSubmissionWebform.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\cluster_api\Assessment;
+
+use Drupal\cluster_api\Assessment\AssessmentSubmissionInterface;
+
+class AssessmentSubmissionWebform implements AssessmentSubmissionInterface {
+
+}
