e6e909a6a1ef8d24400962041501bc20a7d7f7e8
Jean-Michel Lavarenne <jmlavarenne@gmail.com>
2019-01-31 20:39:20 -0500

    SA-4 add map modal to select geocoordinates for webform component

--- docroot/sites/all/modules/custom/cluster_geo/js/geolocation.js
+++ docroot/sites/all/modules/custom/cluster_geo/js/geolocation.js
@@ -1,23 +1,42 @@
 (function ($) {
   Drupal.behaviors.cluster_geo = {
-    attach: function (context, settings) {
-      navigator.geolocation.getCurrentPosition(this.populateWebformFields);
-      this.showMap();
+
+    coordinates: {
+      lat: undefined,
+      lon: undefined,
     },
 
-    populateWebformFields: function(position) {
-      $("#edit-submitted-test-lon").val(position.coords.longitude);
-      $("#edit-submitted-test-lat").val(position.coords.latitude);
-      // console.log(position);
+    attach: function (context, settings) {
+      navigator.geolocation.getCurrentPosition(this.setCoordinatesFromNavigator.bind(this));
+      const cluster_geo = this;
+      $("#modal_opener").click(function() {
+        cluster_geo.showMap();
+      });
+    },
+
+    setCoordinatesFromNavigator: function(position) {
+      this.setCoordinates(position.coords.latitude, position.coords.longitude);
+    },
+
+    setCoordinates: function (lat, lon) {
+      this.coordinates.lat = lat;
+      this.coordinates.lon = lon;
+      this.updateDisplay();
+    },
+
+    updateDisplay: function() {
+      $("#edit-submitted-test-lon").val(this.coordinates.lat);
+      $("#edit-submitted-test-lat").val(this.coordinates.lon);
     },
 
     showMap: function() {
-      var map = L.map('map').setView([51.505, -0.09], 13);
+      const cluster_geo = this;
+      var map = L.map('map').setView([cluster_geo.coordinates.lat, cluster_geo.coordinates.lon], 13);
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map);
       map.on('click', function(e) {
-        alert(e.latlng);
+        cluster_geo.setCoordinates(e.latlng.lat, e.latlng.lng);
       });
     }
   };
---
+++ docroot/sites/all/modules/custom/cluster_geo/template/map-modal.tpl.php
@@ -0,0 +1,10 @@
+<a href="#" id="modal_opener">Choose coordinates from map</a>
+<div class="modal" style="display: none">
+  <div class="overlay"></div>
+  <div class="modal_content">
+    <div id="map" style="height: 480px"></div>
+    <a title="Close" class="close_modal">
+      <i class="fas fa-times"></i>
+    </a>
+  </div>
+</div>
