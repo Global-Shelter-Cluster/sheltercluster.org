b78d9d64d8f6783568943eedb8fbea6c43b8de40
Camilo Bravo <hola@cambraca.com>
2020-02-17 14:49:37 -0500

    SA-137 Home dashboard tweaks

--- docroot/sites/all/modules/custom/cluster_chart/js/charts.js
+++ docroot/sites/all/modules/custom/cluster_chart/js/charts.js
@@ -37,13 +37,29 @@
       if (typeof settings.cluster_chart.type_hbar !== 'undefined') {
         for (var html_id in settings.cluster_chart.type_hbar) {
           $('#' + html_id).once('cluster_charts').each(function() {
+            const options = settings.cluster_chart.type_hbar[html_id].options;
+
+            if (options.scales !== undefined && options.scales.xAxes !== undefined) {
+              for (let i = 0; i < options.scales.xAxes.length; i++) {
+                if (options.scales.xAxes[i].ticks !== undefined && options.scales.xAxes[i].ticks.callback !== undefined) {
+                  switch (options.scales.xAxes[i].ticks.callback) {
+                    case '[M]':
+                      options.scales.xAxes[i].ticks.callback = function(value) {
+                        return (value / 1000000).toFixed(value % 1000000 === 0 ? 0 : 1) + "M";
+                      };
+                      break;
+                  }
+                }
+              }
+            }
+
             new Chart(this, {
               type: 'horizontalBar',
               data: {
                 datasets: settings.cluster_chart.type_hbar[html_id].data,
                 labels: settings.cluster_chart.type_hbar[html_id].labels
               },
-              options: settings.cluster_chart.type_hbar[html_id].options
+              options: options
             })
           });
         }
--- docroot/sites/all/modules/custom/cluster_home/templates/dashboard.tpl.php
+++ docroot/sites/all/modules/custom/cluster_home/templates/dashboard.tpl.php
@@ -1,40 +1,53 @@
 <section class="home-dashboard">
   <div class="header">
-    <h2><?php print t('Currently active Shelter Clusters <small>(or cluster-like mechanisms)</small>'); ?></h2>
+    <h4><?php print t('Currently active Shelter Clusters <small>(or cluster-like mechanisms)</small>'); ?></h4>
     <p><?php print t('A summary of coordination mechanisms in place, more detailed information can be found on the response pages.'); ?></p>
   </div>
-  <div class="active-clusters">
-    <h4><?php print t('Active clusters'); ?></h4>
-    <span class="big-figure"><?php print $count_all_active; ?></span>
+  <div class="row-figures clearfix">
+    <div class="active-clusters">
+      <p><?php print t('Active clusters'); ?></p>
+      <span class="big-figure"><?php print $count_all_active; ?></span>
+    </div>
+    <div class="people-supported">
+      <p><?php print t('People supported'); ?></p>
+      <span
+        class="big-figure"><?php print cluster_factsheets_number($people_supported); ?></span>
+    </div>
+    <div class="partners">
+      <p><?php print t('Cluster partners'); ?></p>
+      <span
+        class="big-figure"><?php print $partners; ?></span>
+    </div>
   </div>
-  <div class="people-supported">
-    <h4><?php print t('People supported'); ?></h4>
-    <span
-      class="big-figure"><?php print cluster_factsheets_number($people_supported); ?></span>
+  <div class="row-pies clearfix">
+    <div class="by-region">
+      <p><?php print t('By region'); ?></p>
+      <?php print render($by_region); ?>
+    </div>
+    <div class="by-disaster-type">
+      <p><?php print t('By type of crisis'); ?></p>
+      <?php print render($by_disaster_type); ?>
+    </div>
   </div>
-  <div class="partners">
-    <h4><?php print t('Cluster partners'); ?></h4>
-    <span
-      class="big-figure"><?php print $partners; ?></span>
+  <div class="row-bars clearfix">
+    <div class="by-country-shelter">
+      <p><?php print t('People supported with shelter'); ?></p>
+      <?php print render($by_country_shelter); ?>
+    </div>
+    <div class="by-country-nfi">
+      <p><?php print t('People supported with NFI'); ?></p>
+      <?php print render($by_country_nfi); ?>
+    </div>
+    <div class="by-country-funding">
+      <p><?php print t('Funding received'); ?></p>
+      <?php print render($by_country_funding); ?>
+    </div>
   </div>
-  <div class="by-region">
-    <h4><?php print t('By region'); ?></h4>
-    <?php print render($by_region); ?>
-  </div>
-  <div class="by-disaster-type">
-    <h4><?php print t('By type of crisis'); ?></h4>
-    <?php print render($by_disaster_type); ?>
-  </div>
-  <div class="by-country-shelter">
-    <h4><?php print t('People supported with shelter'); ?></h4>
-    <?php print render($by_country_shelter); ?>
-  </div>
-  <div class="by-country-nfi">
-    <h4><?php print t('People supported with NFI'); ?></h4>
-    <?php print render($by_country_nfi); ?>
-  </div>
-  <div class="by-country-funding">
-    <h4><?php print t('Funding received'); ?></h4>
-    <?php print render($by_country_funding); ?>
+  <div class="footer">
+    <small>
+      <?php print t('The above data has been made available to the Global Shelter Cluster through factsheets.'); ?>
+      <br/>
+      <?php print t('Achievements from all cluster partners (not just the lead agency) and any form of shelter and/or NFI assistance are counted (except distribution of single items).'); ?>
+    </small>
   </div>
 </section>
