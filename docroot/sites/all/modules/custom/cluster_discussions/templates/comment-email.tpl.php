e49d366765f1a8ee629cfc5d85cfcbe46560d555
Camilo Bravo <hola@cambraca.com>
2020-05-13 14:35:57 -0500

    SA-156 Link to group dashboards from emails

--- docroot/sites/all/modules/custom/cluster_discussions/templates/comment-email.tpl.php
+++ docroot/sites/all/modules/custom/cluster_discussions/templates/comment-email.tpl.php
@@ -6,10 +6,11 @@
   <?php require __DIR__.'/../../cluster_email/templates/notification-header.tpl.php'; ?>
 
   <p style="margin-top: 20px;">
-    <?php print t('A comment has been added to a discussion on <strong>@group</strong>. You can reply directly to this email to add a new comment, or '.
+    <?php print t('A comment has been added to a discussion on <a href="@group_url">@group</a>. You can reply directly to this email to add a new comment, or '.
       '<a href="@url">click here</a> to see it and reply on the Shelter Cluster website.'.
       '<br>You can also create a new discussion on the group by sending an email to <a href="mailto:@email">@email</a>.', [
       '@group' => $group->title,
+      '@group_url' => url('node/'.$group->nid, ['absolute' => TRUE]),
       '@url' => url('node/'.$node->nid, ['absolute' => TRUE, 'fragment' => 'comment-'.$comment->cid]),
       '@email' => cluster_email_inbound_address('discussion-'.$group->nid),
     ]); ?>
